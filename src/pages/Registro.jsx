d37cb04c625a42ba77655f358b83d7a86c0597d9
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-12-11 22:53:09 -0500

    agregado registro de usuario desde el frontend

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { CREATE_USUARIO } from '../graphql/usuario/mutations';
 import '../styles/auth.css';
 
 const Registro = () => {
+  const form = useRef(null);
+
+  const [registro, { data, loading, error }] = useMutation(CREATE_USUARIO);
+
+  if (error) toast.error('Error en el registro');
+  if (loading) {
+    return <div>Registrando...</div>;
+  }
+
+  const crearUsuario = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const nuevoUsuario = {};
+    formData.forEach((item, key) => {
+      nuevoUsuario[key] = item;
+    });
+    registro({ variables: nuevoUsuario })
+      .then(() => {
+        toast.success('Registro Exitoso', { duration: 4000 });
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    e.target.reset();
+  };
   return (
-    <form className="auth-form">
+    <form ref={form} onSubmit={crearUsuario} className="auth-form">
       <h2>Crea tu cuenta!</h2>
       <div className="input-form">
         <i className="fas fa-user"></i>
@@ -20,12 +49,12 @@ const Registro = () => {
       </div>
       <div className="input-form">
         <i className="fas fa-key"></i>
-        <input type="text" placeholder="Contraseña" name="password" required />
+        <input type="password" placeholder="Contraseña" name="password" required />
       </div>
       <div className="input-form">
         <i className="fas fa-user-tag"></i>
         <select name="rol" className="dropdown">
-          <option value="deafult" defaultValue>
+          <option value="deafult" defaultValue disabled>
             Seleccione un rol
           </option>
           <option value="Administrador">Administrador</option>
@@ -40,6 +69,7 @@ const Registro = () => {
       <Link to="/" className="link">
         Inicia Sesión!
       </Link>
+      <Toaster position="top-right" />
     </form>
   );
 };
